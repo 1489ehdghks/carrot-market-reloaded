1b0506d56a42fac9edef76e1621f9da60c76072e
useSword <1489ehdghks@gmail.com>
2025-02-11 22:12:09 +0900

    fix :route 수정

--- lib/session.ts
+++ lib/session.ts
@@ -1,13 +1,26 @@
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
 
-interface SessionContent{
+export interface SessionData {
     id?: number;
 }
 
-export default async function getSession(){
-    return getIronSession<SessionContent>(await cookies(),{
-        cookieName: "LUMI",
-        password: process.env.COOKIE_PASSWORD!,
-    })
+export const sessionOptions = {
+    cookieName: "LUMI",
+    password: process.env.COOKIE_PASSWORD!,
+    cookieOptions: {
+        secure: process.env.NODE_ENV === "production",
+        httpOnly: true, // 클라이언트에서 쿠키 접근 방지
+        sameSite: "lax" as const, // CSRF 보호
+    }
+};
+
+export default async function getSession() {
+    const session =  getIronSession<SessionData>(await cookies(), sessionOptions);
+    
+    if (!process.env.COOKIE_PASSWORD) {
+        throw new Error("COOKIE_PASSWORD is not defined");
+    }
+    
+    return session;
 }
